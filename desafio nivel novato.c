fa4fe667a66eb3ab2be79bb05b1d2e7294742f7a
programacaobento <202501524371@alunos.estacio.br>
2025-03-12 19:58:02 +0000

    correção de alguns dados

--- desafio nivel novato.c
+++ desafio nivel novato.c
@@ -62,16 +62,16 @@ printf("Estado: %s \n", estado1);
 printf("Código da Carta: %s%s \n", estado1,codigo1);
 printf("Nome da Cidade: %s \n", cidade1);
 printf("População:%d \n", populacao1);
-printf("Área:%f Km² \n", area1);
-printf("PIB:%f bilhões de reais \n", pib1);
+printf("Área:%.1f Km² \n", area1);
+printf("PIB:%.1f bilhões de reais \n", pib1);
 printf("Número de pontos turístico: %i \n\v", pontos1);
 printf("Carta 2\n\v");
 printf("Estado: %s \n", estado2);
 printf("Código da Carta: %s%s \n", estado2, codigo2);
 printf("Nome da Cidade: %s \n", cidade2);
 printf("População: %d milhões \n", populacao2);
-printf("Área: %f Km² \n", area2);
-printf("PIB: %f bilhões de reais \n", pib2);
+printf("Área: %.1f Km² \n", area2);
+printf("PIB: %.1f bilhões de reais \n", pib2);
 printf("Número de pontos turístico: %d \n", pontos2);
     
 
